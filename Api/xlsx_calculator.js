39cb408e93d4a6733328e475d9d5b6e5a09d092a
hiikmetgezmen <hikmetgezmen.hg@gmail.com>
2023-06-07 16:03:30 +0300

    Added

--- Api/xlsx_calculator.js
+++ Api/xlsx_calculator.js
@@ -1,7 +1,21 @@
 const XLSX = require("xlsx");
 var Excel = require('exceljs');
-var workbook = new Excel.Workbook();
+const fs = require("fs");
 
+
+
+function writeData(data){
+  const jsonData = JSON.stringify(data);
+  fs.writeFile("data.json", jsonData, (error) => {
+    if (error) {
+      // logging the error
+      console.error(error);
+  
+      throw error;
+    }  console.log("data.json written correctly");
+  });
+
+}
 module.exports = function (file1, file2, file2SheetIndex) {
   const file1SheetName = file1.SheetNames[0];
   const file1Worksheet = file1.Sheets[file1SheetName];
@@ -44,19 +58,41 @@ module.exports = function (file1, file2, file2SheetIndex) {
     }));
 
   const results = [];
+  const result= [];
+
   for (const file1Item of file1DataMapped) {
     const found = file2DataMapped.find(
       (file2Item) => file2Item.id === file1Item.id
     );
     if (!found) continue;
 
-    // const newData = file1Data
-    // .map(function(data){
-    //  data.U = found.value
-    //  return data;
-    // });
+   
+ const total = found.value + file1Item.monday
+     + file1Item.tuesday + file1Item.wednesday + file1Item.thursday + file1Item.friday + file1Item.saturday + file1Item.sunday;
+   const variance =   total - file1Item.value;
+   const variancePercent = ((variance*100) /file1Item.value).toFixed(3) ;
+   const pallet = (-variance)/file1Item.palet;
 
 
+  
+
+   const response =  fs.readFile("data.json", (error, data) => {
+      // if the reading process failed,
+      // throwing the error
+      if (error) {
+        // logging the error
+        console.error(error);
+    
+        throw err;
+      }
+      const user =  JSON.parse(data);
+      for(let i in user){
+        console.log(user[i].id);
+        }
+      // console.log(user);
+    });
+    
+    // console.log(response);
     // const newWb = XLSX.utils.book_new();
     // var newsh = XLSX.utils.json_to_sheet(newData);
     // XLSX.utils.book_append_sheet(newWb,newsh,0);
@@ -78,11 +114,7 @@ module.exports = function (file1, file2, file2SheetIndex) {
 
 
  
-    const total = found.value + file1Item.monday
-     + file1Item.tuesday + file1Item.wednesday + file1Item.thursday + file1Item.friday + file1Item.saturday + file1Item.sunday;
-   const variance =   total - file1Item.value;
-   const variancePercent = ((variance*100) /file1Item.value).toFixed(3) ;
-   const pallet = (-variance)/file1Item.palet;
+   
     // const moreThan5Percent = file1Item.value * 1.05;
     // const lessThan5Percent = file1Item.value * 0.95;
 
@@ -100,7 +132,16 @@ module.exports = function (file1, file2, file2SheetIndex) {
       // result1,
       // result2,
     };
+    const newData = [];
     results.push(result);
+    // for(let id in result){
+    // // results.push(result);
+    //   // results.push({id:id, total:total[id]});
+    // writeData(results);
+
+    // }
+    
   }
+  writeData(results);
   return results;
 };
